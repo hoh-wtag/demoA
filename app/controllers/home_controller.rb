f29bc97dd5aa68efb033bfdadcd66d328834b614
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-11 10:37:01 +0600

    Added flash messages.
    Added flash messages in home page.
    Used flash.now so that message does not move to the next page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
     def index
+        flash.now[:success]="Logged in successfully"; # Using flash.now instead of flash so that the message does not go to the next pasge as cookie.
+        flash[:alert]="Wrong email or password";
     end
 end
