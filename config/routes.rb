7c6cbc5bb1858d2da94569f46777d9fcfce8dcd7
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-11 10:09:25 +0600

    Added Routes, Controller & Views
    Added Routes, Controllers and Views for Homepage and About page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "home#index";
+  #get "about", to: "about#index";
+  get "about-us", to: "about#index", as: :about;
+
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
