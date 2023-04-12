3705ea6d38a970946636f62c1c30a6978c88f6cb
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-12 16:12:00 +0600

    Configured Sign in
    Designed Sign in form
    Added GET and POST routes for sign in
    Configured sessions_controller for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,24 @@
 class SessionsController < ApplicationController
 
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Sign in sucessful"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "Wrong email or password"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     def destroy
         session[:user_id] = nil
         flash[:notice] = 'Signed out'
         redirect_to root_path
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   
   get "signup", to: "registrations#new";
   post "signup", to: "registrations#create";
+  
+  get "signin", to: "sessions#new";
+  post "signin", to: "sessions#create";
 
   delete "signout", to: "sessions#destroy";
 
