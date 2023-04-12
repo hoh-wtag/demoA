2549399c0b3a328f90736dcba46868bc1c8fc323
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-12 13:42:56 +0600

    Configured Sign in with session cookies
    Configured Sign in with session cookies. Users will stay signed in automatically after they sign up.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
     def index
-        flash.now[:success]="Logged in successfully"; # Using flash.now instead of flash so that the message does not go to the next pasge as cookie.
-        flash.now[:alert]="Wrong email or password";
+        #flash.now[:success]="Logged in successfully"; # Using flash.now instead of flash so that the message does not go to the next pasge as cookie.
+        #flash.now[:alert]="Wrong email or password";
+
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id])
+        end
     end
 end
