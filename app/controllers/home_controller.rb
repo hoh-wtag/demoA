ce1e86694c6f37b1b49817bd316fd34fd5e7f8f2
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-16 12:18:28 +0600

    Used Current Attribute for check logged in status
    Created Current.rb model and and made set_current_user method to run before any action in Application Controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
         #flash.now[:success]="Logged in successfully"; # Using flash.now instead of flash so that the message does not go to the next pasge as cookie.
         #flash.now[:alert]="Wrong email or password";
 
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
+        # if session[:user_id]
+        #     @user = User.find_by(id: session[:user_id])
+        # end
     end
 end
